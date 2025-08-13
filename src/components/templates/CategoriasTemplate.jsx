d73bd54347decd9fe448fad277587333599baf0b
Amanda Garcia <amygarcia9618@gmail.com>
2025-08-13 16:26:40 -0400

    commit

--- src/components/templates/CategoriasTemplate.jsx
+++ src/components/templates/CategoriasTemplate.jsx
@@ -1,4 +1,34 @@
+import styled from "styled-components";
 export function CategoriasTemplate(){
-    return (<h1>hola</h1>);
+    return (<Container>
+        <section className="area1">
+
+        </section>
+        <section className="area2">
+            
+        </section>
+        <section className="main">
+
+        </section>
+    </Container>);
 
 }
+const Container = styled.div`
+    height: 100vh;
+    width: 100%;
+    display: grid;
+    grid-template:
+    "area1" 100px
+    "area2" 100px
+    "main" auto;
+    padding: 15px;
+    .area1{
+
+    }
+    .area2{
+
+    }
+    .main{
+        
+    }
+  `;
